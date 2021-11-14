c1519eec0b7cd15092a7995c49b626e3914b14e1
Martín Calderón <martincalderon92@outlook.com>
2021-11-14 20:01:39 -0300

    ✅ Update Search Input test.

--- src/__tests__/SeachInput.test.tsx
+++ src/__tests__/SeachInput.test.tsx
@@ -2,12 +2,12 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('user should navigate to Search page', () => {
+test('user should navigate to Search page', async () => {
 	const { getByRole } = render(<App />);
 	const anchor = getByRole('link', { name: 'Characters' });
 	expect(anchor).toHaveAttribute('href', '/characters&page=1');
 	fireEvent.click(anchor, { click: 0 });
 	userEvent.type(screen.getByPlaceholderText('Search'), 'Spider');
 	userEvent.click(screen.getByRole('button'));
-	expect(screen.getByText(/Spider/i)).toBeTruthy();
+	expect(screen.getByDisplayValue(/Spider/i)).toBeTruthy();
 });
