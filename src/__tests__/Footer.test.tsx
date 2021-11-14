eb11ac306c3d112f83465a57c85b2a32a01c37d7
Martín Calderón <martincalderon92@outlook.com>
2021-11-14 00:34:41 -0300

    :recycle: Change variable name in test of Footer.

--- src/__tests__/Footer.test.tsx
+++ src/__tests__/Footer.test.tsx
@@ -3,6 +3,6 @@ import Footer from '../components/Footer';
 
 test('should be rendered', () =>   {
     const { getByText } = render(<Footer />);
-    const linkElement = getByText(/All rights reserved/i);
-    expect(linkElement).toBeInTheDocument();
+    const textElement = getByText(/All rights reserved/i);
+    expect(textElement).toBeInTheDocument();
 })
